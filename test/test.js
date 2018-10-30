5a6bf5cc13ed472a0fe08589004f954baa8f165d
graykode <nlkey2022@gmail.com>
2018-10-31 00:10:36 +0900

    #6 unit test

--- test/test.js
+++ test/test.js
@@ -1,16 +1,38 @@
-var app = require('./app');
+var app = require('../app');
 var request = require('supertest');
 var expect = require('expect.js');
 
-describe('GET /users', function(){
-    it('respond users json', function(done){
+describe('POST /add', function(){
+    it('respond tasks json', function(done){
         request(app)
-            .get('/')
-            .expect(200)
+            .post('/add')
+            .send('title=운동하기&description=수영 왕복 1km&priority=1')
+            .expect(302)
             .end(function(err, res) {
                 if(err) throw err;
 
-                expect(res.body).to.be.an(Array);
+                done();
+            });
+    });
+    it('respond tasks json', function(done){
+        request(app)
+            .post('/add')
+            .send('title=공부&description=알고리즘 공부&priority=2&deadline=10/22/2018 12:00 AM')
+            .expect(302)
+            .end(function(err, res) {
+                if(err) throw err;
+
+                done();
+            });
+    });
+    it('respond tasks json', function(done){
+        request(app)
+            .post('/add')
+            .send('title=운동하기&description=농구 30분&priority=3')
+            .expect(302)
+            .end(function(err, res) {
+                if(err) throw err;
+
                 done();
             });
     });
